5ad781838a5585c8f5666c9e2ea73c730ff6809f
fat7llah92 <88618582+fat7llah92@users.noreply.github.com>
2025-07-13 14:37:49 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // <- required if you're using app directory
   ],
   theme: {
     extend: {},
