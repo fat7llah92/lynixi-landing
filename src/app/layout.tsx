d77ce513abdeebe7f56473270a800fda5ddb2bde
fat7llah92 <88618582+fat7llah92@users.noreply.github.com>
2025-07-13 14:39:54 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ export const metadata = {
   title: "Lynixi",
   description: "Cybersecurity Talent Marketplace",
 };
-
+import './globals.css'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
